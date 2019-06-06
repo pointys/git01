1e2c94a807346338f25cb3617342ed6b9f640465
pointys <1924552207@qq.com>
2019-06-06 13:50:50 +0800

    增加了付款查询接口

---
+++ src/com/nm/controller/PeopleController.java
@@ -0,0 +1,11 @@
+package com.nm.controller;
+
+/**
+ * @Author: 韩老魔
+ * @Date: 2019/6/6 0006 13:49
+ */
+public class PeopleController {
+    public void queryRecord(){
+        System.out.println("控制器");
+    }
+}
