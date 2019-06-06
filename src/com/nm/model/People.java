bd1282cd8588006089176b973e4ca5ddb99dbff8
pointys <1924552207@qq.com>
2019-06-06 12:10:16 +0800

    编写了model

---
+++ src/com/nm/model/People.java
@@ -0,0 +1,35 @@
+package com.nm.model;
+
+/**
+ * @Author: 韩老魔
+ * @Date: 2019/6/6 0006 12:06
+ */
+public class People {
+    private Integer uid;
+    private String name;
+    private String password;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
